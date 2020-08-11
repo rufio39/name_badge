cbffb31e930f7d05edcbf3bf604d68e6af4e156d
rufio39 <69258446+rufio39@users.noreply.github.com>
2020-08-11 09:07:59 -0600

    initial commit

---
+++ nameform.js
@@ -0,0 +1,7 @@
+function changeName() {
+    let inputName = document.getElementById("username")
+    let badgeName = document.getElementById("badgeName")
+
+    badgeName.innerHTML = inputName.value
+
+}
